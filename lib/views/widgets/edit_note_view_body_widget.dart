9b1313d6d35b97c70b4ca57140d8a5f74d2db231
MohammedGhazaly <105166510+MohammedGhazaly@users.noreply.github.com>
2023-04-20 09:48:12 +0200

    adding form fields to edit note view body

--- lib/views/widgets/edit_note_view_body_widget.dart
+++ lib/views/widgets/edit_note_view_body_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/views/widgets/custom_app_bar_widget.dart';
+import 'package:notes_app/views/widgets/custom_text_field_widget.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -21,6 +22,17 @@ class EditNoteViewBody extends StatelessWidget {
               CustomAppBar(
                 title: "Edit Note",
                 icon: FontAwesomeIcons.check,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              CustomTextFieldWidget(hintText: "Title"),
+              SizedBox(
+                height: 16,
+              ),
+              CustomTextFieldWidget(
+                hintText: "Content",
+                maxLines: 5,
               )
             ],
           ),
